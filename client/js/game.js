4e8ca0197ead42aca6b29c9b1913d04cd4bb47f0
Aske Færch Christensen <askefc@gmail.com>
2020-11-01 23:50:15 +0100

    Update
    Add a bunch of starry backgrounds.
    Add background changing for all players every 2 minutes.
    Code optimisations.
    Use ecma 7 syntax.
    Add text outlines to allow visibility on various backgrounds.
    Use resources only from CDN with SRI.
    Let variables be sent on first clikent connect instead of by ajax.
    Upgrade all resource versions and use smallest possible.
    Tweak Phaser to show very clear canvas on hidpi screens as well.
    Add subtle grid on world, so players can navigate on various backgrounds.

--- client/js/game.js
+++ client/js/game.js
@@ -1,51 +1,64 @@
-let MAP_WIDTH = 500;
-let MAP_HEIGHT = 500;
-
 const PIXEL_SIZE = 10;
-const CAMERA_SPEED = 0.15;
+const CAMERA_SPEED = 0.20;
 
-var PLAYER_ID = -1;
+let PLAYER_ID = -1;
 
-var game;
-var socket = io();
+let game;
+const socket = io();
 
-var cameraFollow;
+let cameraFollow;
 
-var players;
-var tails;
-var food;
-var map;
-var names;
+let players;
+let tails;
+let food;
+let map;
+let names;
+let backgroundSprite;
+
+let elements = {};
 
 /* Server ping */
-var startTime;
+let startTime;
 
-setInterval(function() {
+setInterval(() => {
   startTime = Date.now();
   socket.emit('ping2');
 }, 2000);
 
-socket.on('pong2', function() {
+socket.on('pong2', () => {
 	let latency = Date.now() - startTime;
     const fast = (latency < 100);
-    const pingBadge = document.querySelector("#ping-badge");
-    pingBadge.classList.remove((fast ? "badge-danger" : "badge-success"));
-    pingBadge.classList.add((fast ? "badge-success" : "badge-danger"));
-	document.querySelector("#server-ping").textContent = latency;
+    elements.pingBadge.classList.remove((fast ? 'badge-danger' : 'badge-success'));
+    elements.pingBadge.classList.add((fast ? 'badge-success' : 'badge-danger'));
+	elements.serverPing.textContent = latency;
 });
 
 /* Init game engine*/
-function preload() {
-	game.load.image('background', '/client/img/game/background.png');
+const preload = () => {
+	game.load.image('background1', '/client/img/game/background.png');
+	game.load.image('background2', '/client/img/game/star1.jpg');
+	game.load.image('background3', '/client/img/game/star2.jpg');
+	game.load.image('background4', '/client/img/game/star3.jpg');
+	game.load.image('background5', '/client/img/game/star4.jpg');
+	game.load.image('background6', '/client/img/game/star5.jpg');
+	game.load.image('background7', '/client/img/game/star6.jpg');
+	game.load.image('background8', '/client/img/game/star7.jpg');
+	game.load.image('background9', '/client/img/game/star8.jpg');
+	game.load.image('background10', '/client/img/game/star9.jpg');
+	game.load.image('background11', '/client/img/game/star10.jpg');
+	game.load.image('background12', '/client/img/game/star11.jpg');
 };
 
-function create() {
-	game.world.setBounds(0, 0, MAP_WIDTH * PIXEL_SIZE, MAP_HEIGHT * PIXEL_SIZE);
-	game.add.tileSprite(0, 0, game.width * PIXEL_SIZE, game.height * PIXEL_SIZE, "background");
+const create = () => {
+    game.stage.smoothed = false;
+    game.world.setBounds(0, 0, MAP_WIDTH * PIXEL_SIZE, MAP_HEIGHT * PIXEL_SIZE);
 
-	game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
-	game.scale.fullScreenScaleMode = Phaser.ScaleManager.RESIZE;
-	game.scale.parentIsWindow = true;
+    const ratioPixelSize = PIXEL_SIZE * window.devicePixelRatio;
+	backgroundSprite = game.add.tileSprite(0, 0, MAP_WIDTH * ratioPixelSize, MAP_HEIGHT * ratioPixelSize, 'background' + BACKGROUND_ID);
+    backgroundSprite.alpha = 1;
+    game.create.grid('grid', MAP_WIDTH * ratioPixelSize, MAP_HEIGHT * ratioPixelSize, ratioPixelSize, ratioPixelSize, 'rgba(255,255,255,0.2)', true, () => { game.add.sprite(0, 0, 'grid'); });
+
+	game.scale.parentIsWindow = false;
 
 	cameraFollow = game.add.sprite(game.world.centerX, game.world.centerY);
 
@@ -69,29 +82,31 @@ function create() {
 	g.drawRect(0, (MAP_HEIGHT - 1) * PIXEL_SIZE, MAP_WIDTH * PIXEL_SIZE, MAP_HEIGHT * PIXEL_SIZE);
 	g.drawRect((MAP_WIDTH - 1) * PIXEL_SIZE, 0, (MAP_HEIGHT) * PIXEL_SIZE, MAP_HEIGHT * PIXEL_SIZE);
 	g.endFill();
+};
 
-	map.add(g);
+const update = () => {
+
 };
 
 /* Socket events */
-socket.on("id", function(data) {
+socket.on('id', (data) => {
 	PLAYER_ID = data.id;
-	console.log("Your id is " + PLAYER_ID);
+	console.log('Your id is ' + PLAYER_ID);
 });
 
-socket.on("death", function(data) {
-	document.querySelector("#total-score").textContent = data.score;
-	document.querySelector("#final-score").style.display = "block";
-	setTimeout(function() {
-		$(document.querySelector("#menu")).fadeIn(1000);
-		$(document.querySelector("#player-info")).fadeOut(1000);
-		document.querySelector("#btn_play").focus();
+socket.on('death', (data) => {
+	elements.totalScore.textContent = data.score;
+	elements.finalScore.style.display = 'block';
+    setTimeout(() => {
+		$(elements.menu).fadeIn(1000);
+		$(elements.playerInfo).fadeOut(1000);
+		elements.btnPlay.focus();
 	}, 1000);
 });
 
-socket.on("spawn", function(data) {
-	$(document.querySelector("#menu")).fadeOut(500);
-	$(document.querySelector("#player-info")).fadeIn(500);
+socket.on('spawn', (data) => {
+	$(elements.menu).fadeOut(500);
+	$(elements.playerInfo).fadeIn(500);
 	try {
 		game.camera.follow(null, Phaser.Camera.FOLLOW_LOCKON, 1, 1);
 		game.camera.x = data.x * PIXEL_SIZE;
@@ -102,9 +117,9 @@ socket.on("spawn", function(data) {
 	};
 });
 
-socket.on("gamestate", function(data) {
-	if(players == undefined || tails == undefined || food == undefined || names == undefined) {
-		console.log("Waiting for engine to start...");
+socket.on('gamestate', (data) => {
+    if (players == undefined || tails == undefined || food == undefined || names == undefined) {
+		console.log('Waiting for engine to start...');
 		return;
 	};
 
@@ -114,14 +129,14 @@ socket.on("gamestate", function(data) {
 	names.removeAll();
 
     let leaderboardcontent = "";
-	while(data.leaderboard.length > 0) {
+	while (data.leaderboard.length > 0) {
 		let entry = data.leaderboard.pop();
-		leaderboardcontent += '<div class="lb-entry ' + ((entry.id == PLAYER_ID) ? "lb-entry-self" : "") + '">' + (entry.place + 1) + ': ' + encodeHTML(entry.name) + '</div>';
+		leaderboardcontent += '<div class="lb-entry ' + ((entry.id === PLAYER_ID) ? 'lb-entry-self' : '') + '">' + (entry.place + 1) + ': ' + encodeHTML(entry.name) + '</div>';
 	};
 
-	document.querySelector("#leaderboard-content").innerHTML = leaderboardcontent;
+	elements.leaderboardContent.innerHTML = leaderboardcontent;
 
-	for(let i = 0, iEnd = data.food.length; i < iEnd; ++i) {
+	for (let i = 0, iEnd = data.food.length; i < iEnd; ++i) {
 		let foodData = data.food[i];
 		let g = game.add.graphics(foodData.x * PIXEL_SIZE, foodData.y * PIXEL_SIZE);
 		g.beginFill(hslToHex(foodData.color, 100, 35), 1);
@@ -130,7 +145,7 @@ socket.on("gamestate", function(data) {
 		food.add(g);
 	};
 
-	for(let i = 0, iEnd = data.playerTails.length; i < iEnd; ++i) {
+	for (let i = 0, iEnd = data.playerTails.length; i < iEnd; ++i) {
 		let tail = data.playerTails[i];
 		let g = game.add.graphics(tail.x * PIXEL_SIZE, tail.y * PIXEL_SIZE);
 		g.beginFill(hslToHex(tail.color, 100, 25), 1);
@@ -139,15 +154,15 @@ socket.on("gamestate", function(data) {
 		tails.add(g);
 	};
 
-	for(let i = 0, iEnd = data.players.length; i < iEnd; ++i) {
+	for (let i = 0, iEnd = data.players.length; i < iEnd; ++i) {
 		let player = data.players[i];
 		let g = game.add.graphics(player.x* PIXEL_SIZE, player.y * PIXEL_SIZE);
 
-		if(player.id === PLAYER_ID) {
+		if (player.id === PLAYER_ID) {
 			cameraFollow.x = (player.x * PIXEL_SIZE);
 			cameraFollow.y = (player.y * PIXEL_SIZE);
-			document.querySelector("#player-score").textContent = player.score;
-			document.querySelector("#position").textContent = "X: " + player.x + " Y: " + player.y;
+			elements.playerScore.textContent = player.score;
+			elements.position.textContent = "X: " + player.x + " Y: " + player.y;
 		};
 
 		g.beginFill(hslToHex(player.color, 100, 50), 1);
@@ -155,85 +170,108 @@ socket.on("gamestate", function(data) {
 		g.endFill();
 		players.add(g);
 
-		let t = game.add.text(player.x * PIXEL_SIZE, (player.y * PIXEL_SIZE) - 10, player.name, {fill:"#000", fontSize:"15px"});
+		let t = game.add.text(player.x * PIXEL_SIZE, (player.y * PIXEL_SIZE) - 10, player.name, {fill: '#FFF', fontSize: '16px', stroke: '#000', strokeThickness: 1});
 		t.anchor.setTo(0.5);
+        t.smoothed = false;
+        t.resolution = window.devicePixelRatio;
 		names.add(t);
 	};
 });
 
+socket.on('backgroundUpdate', (data) => {
+    BACKGROUND_ID = data.BACKGROUND_ID;
+    game.add.tween(backgroundSprite).to({ alpha: 0 }, 1000, Phaser.Easing.Linear.None, true, 0, 0, false);
+    window.setTimeout(() => {
+        backgroundSprite.loadTexture('background' + BACKGROUND_ID);
+        game.add.tween(backgroundSprite).to({ alpha: 1 }, 1000, Phaser.Easing.Linear.None, true, 0, 0, false);
+    }, 1000);
+});
+
 /* Functions */
-function encodeHTML(s) {
+const encodeHTML = (s) => {
 	return s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
 };
 
-function componentToHex(c) {
-	var hex = c.toString(16);
-	return hex.length === 1 ? "0" + hex : hex;
+const componentToHex = (c) => {
+	const hex = c.toString(16);
+	return hex.length === 1 ? '0' + hex : hex;
 };
 
-function hslToHex(h,s,l) {
-	let rgb = Phaser.Color.HSLtoRGB(h / 360, s / 100, l / 100);
-	return "0x"+componentToHex(rgb.r) + componentToHex(rgb.g) + componentToHex(rgb.b);
+const hslToHex = (h,s,l) => {
+	const rgb = Phaser.Color.HSLtoRGB(h / 360, s / 100, l / 100);
+	return '0x'+componentToHex(rgb.r) + componentToHex(rgb.g) + componentToHex(rgb.b);
 };
 
-function rgbToHex(r, g, b) {
-	return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+const rgbToHex = (r, g, b) => {
+	return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b);
 };
 
-function play() {
-	socket.emit("spawn", {name:document.querySelector("#name").value});
+const play = () => {
+	socket.emit('spawn', {name: elements.name.value});
 };
 
 /* Load */
-document.addEventListener("DOMContentLoaded", function() {
-	try {
-		let conf = JSON.parse($.ajax({
-			async: false,
-			cache: false,
-			type: "GET",
-			url: "/config"
-		}).responseText);
+document.addEventListener('DOMContentLoaded', () => {
+    elements = {
+        playerInfo: document.querySelector('#player-info'),
+        playerScore: document.querySelector('#player-score'),
+        finalScore: document.querySelector('#final-score'),
+        totalScore: document.querySelector('#total-score'),
+        btnPlay: document.querySelector('#btn_play'),
+        form: document.querySelector('form'),
+        name: document.querySelector('#name'),
+        menu: document.querySelector('#menu'),
+        position: document.querySelector('#position'),
+        leaderboardContent: document.querySelector('#leaderboard-content'),
+        serverPing: document.querySelector('#server-ping'),
+        pingBadge: document.querySelector('#ping-badge'),
+        snakeGame: document.querySelector("#snake-game")
+    };
 
-		MAP_WIDTH = conf.MAP_WIDTH;
-		MAP_HEIGHT = conf.MAP_HEIGHT;
-		document.querySelector("#name").setAttribute('maxlength', conf.MAX_NAME_LENGTH);
-	} catch(err) {
-		console.log(err);
-	};
-	document.querySelector("#final-score").style.display = "none";
-	document.querySelector("#btn_play").addEventListener("click", function() {
+	elements.name.focus();
+	elements.finalScore.style.display = 'none';
+
+    elements.btnPlay.addEventListener('click', () => {
 		play();
-	});
+	}, {capture: true, once: false, passive: true});
 
-	document.querySelector("form").addEventListener('submit',function(e){
+	elements.form.addEventListener('submit',(e) => {
 		e.preventDefault();
 		play();
-	});
+	}, {capture: true, once: false, passive: false});
 
-    document.querySelector("#name").addEventListener("change", function() {
-        setCookie("MultiplayerSnake-name", document.querySelector("#name").value, 14);
-    });
+    elements.name.addEventListener('change', () => {
+        setCookie('MultiplayerSnake-name', elements.name.value, 14);
+    }, {capture: true, once: false, passive: true});
 
-	document.querySelector("#name").focus();
-
-	// game = new Phaser.Game(800, 600, Phaser.CANVAS, 'snake-game', {preload: preload, create:create});
 	game = new Phaser.Game({
-        type: Phaser.AUTO,
-        mode: Phaser.Scale.FIT,
-        parent: 'snake-game',
-        width: 800,
-        height: 600,
-        scene: {
+        width: elements.snakeGame.clientWidth,
+        height: elements.snakeGame.clientHeight,
+        renderer: Phaser.CANVAS,
+        parent: elements.snakeGame,
+        transparent: false,
+        antialias: false,
+        multiTexture: false,
+        backgroundColor: 'rgba(0,0,0,1)',
+        clearBeforeRender: true,
+        crisp: true,
+        enableDebug: false,
+        fullScreenScaleMode: Phaser.ScaleManager.RESIZE,
+        resolution: window.devicePixelRatio,
+        roundPixels: true,
+        scaleMode: Phaser.ScaleManager.RESIZE,
+        state: {
             preload: preload,
-            create: create
+            create: create,
+            update: update
         }
     });
 
 	try {
-		let name = getCookie("MultiplayerSnake-name");
-		if(name.length > 0 && name.length <= 16) {
-			console.log("Loaded name from cookie: " + name);
-			document.querySelector("#name").value = name;
+		let name = getCookie('MultiplayerSnake-name');
+		if (name.length > 0 && name.length <= 16) {
+			console.log('Loaded name from cookie: ' + name);
+			elements.name.value = name;
 		};
 	} catch(err) {
 		console.log(err);
@@ -241,7 +279,7 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 /* Key listener */
-document.addEventListener("keydown", function(e) {
+document.addEventListener('keydown', (e) => {
 	const key = (e === null) ? event.keyCode : e.which;
 
     let inputId = null;
@@ -270,4 +308,4 @@ document.addEventListener("keydown", function(e) {
 			state: true
 		});
 	};
-});
+}, {capture: false, once: false, passive: true});
